0e7084fd6b8e1721d9b9ff846720a48fe85c8287
liujie <liujie5607@163.com>
2020-06-05 17:42:43 +0800

    init

--- src/JavaLab.java
+++ src/JavaLab.java
@@ -1,12 +1,3 @@
 public class JavaLab {
 
-    public static void add(int sum) {
-        sum++;
-    }
-
-    public static void main(String[] args) {
-        int a = 10;
-        add(a);
-        System.out.println(a);
-    }
 }
--- src/com/leetcode/tree/P501.java
+++ src/com/leetcode/tree/P501.java
@@ -7,8 +7,45 @@ import java.util.List;
 public class P501 {
 
     class Solution {
+
+        int occurCurTimes = 1;
+        int occurMaxTimes = 1;
+
+        TreeNode preNode = null;
+
+        List<TreeNode> res = new ArrayList<>();
+
         public int[] findMode(TreeNode root) {
-            return null;
+            InOrderTravel(root);
+            int[] modes = new int[res.size()];
+            for (int i = 0; i < res.size(); i++) {
+                modes[i] = res.get(i).val;
+            }
+            return modes;
+        }
+
+        public void InOrderTravel(TreeNode node) {
+
+            if (node == null) return;
+            InOrderTravel(node.left);
+            if (preNode != null) {
+                if (node.val == preNode.val) {
+                    occurCurTimes++;
+                }
+                else {
+                    if (occurCurTimes == occurMaxTimes) {
+                        res.add(node);
+                    }
+                    else if (occurCurTimes > occurMaxTimes) {
+                        occurMaxTimes = occurCurTimes;
+                        res.clear();
+                        res.add(node);
+                    }
+                    occurCurTimes = 1;
+                }
+            }
+            preNode = node;
+            InOrderTravel(node.right);
         }
     }
 }
