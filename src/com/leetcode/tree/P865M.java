af55bc41f27a1dcc07e72cdad88ec348f9df85e4
liujie <liujie5607@163.com>
2020-06-16 14:30:53 +0800

    tree  Media prcocess saved！

--- src/com/leetcode/tree/P865M.java
+++ src/com/leetcode/tree/P865M.java
@@ -2,6 +2,23 @@ package com.leetcode.tree;
 
 public class P865M {
     class Solution {
+        public TreeNode subtreeWithAllDeepest(TreeNode root) {
+            int ldepth = depth(root.left);
+            int rdepth = depth(root.right);
 
+            if (ldepth == rdepth)
+                return root;
+            if (ldepth > rdepth)
+                return subtreeWithAllDeepest(root.left);
+            else
+                return subtreeWithAllDeepest(root.right);
+        }
+
+        public int depth(TreeNode node) {
+            if (node == null)
+                return 0;
+            else
+                return Math.max(depth(node.left), depth(node.right)) + 1;
+        }
     }
 }
