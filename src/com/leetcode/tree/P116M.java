e8e1d510b4e6ab82b7eb4ae2decc93b820befc40
liujie <liujie5607@163.com>
2020-06-18 08:39:10 +0800

    tree  Media prcocess saved！

--- src/com/leetcode/tree/P116M.java
+++ src/com/leetcode/tree/P116M.java
@@ -23,7 +23,17 @@ public class P116M {
 
     class Solution {
         public Node connect(Node root) {
-            return null;
+            if(root==null) return null;
+            if(root.left!=null)
+                root.left.next = root.right;
+            if(root.right!=null) {
+                if(root.next!=null) {
+                    root.right.next = root.next.left;
+                }
+            }
+            connect(root.left);
+            connect(root.right);
+            return root;
         }
     }
 }
