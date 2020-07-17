fb812696e2e1a61a979385bf9120a951dbe9c9d8
liujie <liujie5607@163.com>
2020-07-17 17:47:30 +0800

    tree  Media prcocess saved！

--- src/com/leetcode/linkedlist/P160.java
+++ src/com/leetcode/linkedlist/P160.java
@@ -4,7 +4,7 @@ public class P160 {
     public class Solution {
         public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
             ListNode pa=headA,pb=headB;
-
+            return null;
         }
     }
 }
