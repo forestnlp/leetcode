794fc66fef43aed53646bc074fcf854f73be8d95
liujie <liujie5607@163.com>
2020-08-17 17:35:26 +0800

    tree  Media prcocess saved！

--- src/com/leetcode/sort/P1030.java
+++ src/com/leetcode/sort/P1030.java
@@ -3,7 +3,7 @@ package com.leetcode.sort;
 public class P1030 {
     class Solution {
         public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
-            
+            return null;
         }
     }
 }
