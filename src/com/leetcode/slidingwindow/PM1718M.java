f521d54cdb1711fc30e2d10825434a3d9c8e284b
liujie <liujie5607@163.com>
2020-11-30 10:40:20 +0800

    daily task or training  saved！

---
+++ src/com/leetcode/slidingwindow/PM1718M.java
@@ -0,0 +1,23 @@
+package com.leetcode.slidingwindow;
+
+public class PM1718M {
+    static class Solution {
+        public int[] shortestSeq(int[] big, int[] small) {
+
+            int left=0,right=0;
+
+
+
+            while(right<big.length) {
+
+                //extends right cursor
+
+            }
+            return new int[0];
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
